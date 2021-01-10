42427f9827b1f00c2796b1099b2ba9b72d24bad5
hurricane67 <azahaf.zakariae@gmail.com>
2021-01-10 19:13:32 +0100

    initial commit

---
+++ src/test/java/zak/sfg/joke/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package zak.sfg.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
